257f69a09d907598df0eb7090861f897bd0d56ea
Natasha Cleveland <59803795+nclevel1@users.noreply.github.com>
2022-04-27 10:34:46 -0400

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import config as cfg
-import pandas as pd
-import numpy as np
-from pathlib import Path
-from datetime import datetime
-from datetime import timedelta
-from googleapiclient.discovery import build
